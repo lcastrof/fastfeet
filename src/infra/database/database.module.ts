5fa7b9b505b23a0f87e6efca3b5ea6a34ad1770c
Lucas de Castro <lucascastrofr@gmail.com>
2024-05-14 23:08:48 -0300

    chore: add todo comment as a reminder

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmRootModuleConfig } from "./typeorm/config";
 
+// TODO - Implement Custom Repositories and put them in the providers and exports array
+// so that they can be injected into services when this module is imported
 @Module({
   imports: [TypeOrmRootModuleConfig],
   providers: [],
